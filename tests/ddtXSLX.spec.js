52315e1b04c90d85e63e59d8a0e788a58e5897dc
PAPUL <paridapapul5@gmail.com>
2026-06-25 11:29:18 +0530

    commit message

--- tests/ddtXSLX.spec.js
+++ tests/ddtXSLX.spec.js
@@ -1,28 +1,3 @@
-/*import{test,expect} from "@playwright/test";
-import fs from 'fs';
-import * as XLSX from "xlsx";
-
-const xlsxFilePath="TestData/testData.xlsx";
-const content = XLSX.readFile(xlsxFilePath);
-const sheet = content.SheetNames[0];
-const worksheet = content.Sheets[sheet]
-
-const loginData= XLSX.utils.sheet_to_json(worksheet);
-
-for(let {email , password} of loginData) {
-  test(`webshop login test for ${email}`, async({page})=>{
-    await page.goto("https://demowebshop.tricentis.com/");
-    await page.locator(".ico-login").click();
-    await page.locator("#Email").fill(email);
-    await page.locator("#Password").fill(password);
-    await page.locator(".login-button").click();
-    let logged = await page.getByRole("link" , {name :"Log out"});
-    expect(logged).toHaveText("Log out");
-  
-
-
-  })
-}*/
 
 import { test, expect } from "@playwright/test";
 import * as XLSX from "xlsx";
